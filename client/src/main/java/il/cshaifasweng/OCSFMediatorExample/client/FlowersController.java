7919789ad7b4eff1d259d28c1f407c36619aff12
Saherdaoud <sdaoud03@campus.haifa.ac.il>
2022-04-28 17:57:30 +0300

    laaaaaaaaaaaastttttttttttttt

--- client/src/main/java/il/cshaifasweng/OCSFMediatorExample/client/FlowersController.java
+++ client/src/main/java/il/cshaifasweng/OCSFMediatorExample/client/FlowersController.java
@@ -1,13 +1,18 @@
 package il.cshaifasweng.OCSFMediatorExample.client;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Button;
 
 import il.cshaifasweng.OCSFMediatorExample.entities.Catalog;
 import il.cshaifasweng.OCSFMediatorExample.entities.Item;
 import javafx.fxml.FXML;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.text.Text;
 
 public class FlowersController {
@@ -21,6 +26,9 @@ public class FlowersController {
     public static void setItem(Item item) {
         FlowersController.item = item;
     }
+
+    @FXML
+    private AnchorPane Pane1;
 
     @FXML
     private ResourceBundle resources;
@@ -50,8 +58,19 @@ public class FlowersController {
     private Text priceee;
 
     @FXML
+    private Button back_to_catalog;
+
+    @FXML
+    void back_to_catalog1(ActionEvent event) throws IOException {
+        AnchorPane pane = FXMLLoader.load(getClass().getResource("homepage.fxml"));
+        Pane1.getChildren().setAll(pane);
+    }
+
+    @FXML
     void initialize() {
         System.out.format("5555566665");
+        assert Pane1 != null : "fx:id=\"Pane1\" was not injected: check your FXML file 'flowers.fxml'.";
+        assert back_to_catalog != null : "fx:id=\"back_to_catalog\" was not injected: check your FXML file 'flowers.fxml'.";
         assert flower_color != null : "fx:id=\"flower_color\" was not injected: check your FXML file 'flowers.fxml'.";
         assert flower_id != null : "fx:id=\"flower_id\" was not injected: check your FXML file 'flowers.fxml'.";
         assert flower_image != null : "fx:id=\"flower_image\" was not injected: check your FXML file 'flowers.fxml'.";
